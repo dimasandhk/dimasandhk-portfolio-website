8a98c94a6dc91b98046bd27d7e35eb95c08f9908
Dimas Andhika Diputra <dimasandhikadiputra@gmail.com>
2026-04-11 21:05:13 +0700

    feat: add youtube api referer

--- src/lib/server/youtube.ts
+++ src/lib/server/youtube.ts
@@ -1,9 +1,16 @@
 import { env } from '$env/dynamic/private';
+import { dev } from '$app/environment';
 
 export const getLatestVideos = async () => {
 	const apiKey = env.YOUTUBE_API_KEY;
 	const channelId = env.YOUTUBE_CHANNEL_ID;
-	const url = `https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=${channelId}&maxResults=2&order=date&type=video&key=${apiKey}`;
 
-	return fetch(url);
+	const url = `https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=${channelId}&maxResults=2&order=date&type=video&key=${apiKey}`;
+	const referer = dev ? 'http://localhost:5173' : 'https://dimasandhika.vercel.app/';
+
+	return fetch(url, {
+		headers: {
+			Referer: referer
+		}
+	});
 };
